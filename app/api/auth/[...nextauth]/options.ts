1743122fe556403429a5d7bf97581228239b8a21
Anish De <63192115+AnishDe12020@users.noreply.github.com>
2023-10-20 12:31:35 +0530

    feat: user signup

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -2,6 +2,8 @@ import { NextRequest } from "next/server"
 import type { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 
+import prisma from "@/lib/prisma"
+
 export const options = (req: NextRequest): NextAuthOptions => {
   return {
     providers: [
@@ -11,8 +13,22 @@ export const options = (req: NextRequest): NextAuthOptions => {
       }),
     ],
     events: {
-      signIn: (message) => {
+      signIn: async (message) => {
         console.log(message)
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: message.user.email as string,
+          },
+        })
+
+        if (!user) {
+          await prisma.user.create({
+            data: {
+              email: message.user.email as string,
+            },
+          })
+        }
       },
     },
   }
