14a42d88cd318728eaaefd2c68c7f90bbefe9df2
Anish De <63192115+AnishDe12020@users.noreply.github.com>
2023-10-21 09:56:56 +0530

    feat: manage page

--- app/dashboard/orgs/[id]/page.tsx
+++ app/dashboard/orgs/[id]/page.tsx
@@ -1,6 +1,14 @@
 import { notFound } from "next/navigation"
 
 import prisma from "@/lib/prisma"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
 const OrgPage = async ({
   params: { id },
@@ -13,6 +21,13 @@ const OrgPage = async ({
     where: {
       id: parseInt(id),
     },
+    include: {
+      nfts: {
+        include: {
+          user: true,
+        },
+      },
+    },
   })
 
   console.log(org)
@@ -23,9 +38,35 @@ const OrgPage = async ({
 
   return (
     <>
-      <div className="flex gap-4 items-center">
-        <img src={org.logoUrl} className="w-10 h-10 rounded-lg" />
-        <h1>{org.name}</h1>
+      <div className="flex flex-col gap-8">
+        <div className="flex gap-4 items-center">
+          <img src={org.logoUrl} className="w-10 h-10 rounded-lg" />
+          <h1>{org.name}</h1>
+        </div>
+        <div className="flex flex-col gap-4">
+          {org.nfts.length > 0 ? (
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Email</TableHead>
+                  <TableHead>Points</TableHead>
+                  <TableHead>Tier</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {org.nfts.map((nft) => (
+                  <TableRow>
+                    <TableCell>{nft.user.email}</TableCell>
+                    <TableCell>{nft.points}</TableCell>
+                    <TableCell>{nft.tier}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          ) : (
+            <p>No NFTs rewarded yet</p>
+          )}
+        </div>
       </div>
     </>
   )
