c15afd5f55083b9901220f22ef6e68295e8668a0
Anish De <63192115+AnishDe12020@users.noreply.github.com>
2023-10-21 09:52:26 +0530

    chore: small hero

--- app/page.tsx
+++ app/page.tsx
@@ -12,20 +12,21 @@ export default function IndexPage() {
   console.log(session)
 
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with Radix UI and Tailwind CSS.
-        </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground">
-          Accessible and customizable components that you can copy and paste
-          into your apps. Free. Open Source. And Next.js 13 Ready.
-        </p>
-      </div>
-      <div className="flex gap-4">
-        <Button onClick={() => signIn("google")}>Sign In</Button>
-      </div>
-    </section>
+    <div className="flex flex-col items-center min-h-screen py-2">
+      <section className="div flex gap-32 justify-between max-w-5xl mt-16">
+        <div className="flex max-w-[980px] flex-col items-start gap-2">
+          <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+            NFT Loyalty program on Solana
+          </h1>
+          <p className="max-w-[700px] text-lg text-muted-foreground">
+            Ardent protocol is a loyalty program that allows you to reward your
+            customers with NFTs which can futher unlock benefits like discounts,
+            access to exclusive events and more.
+          </p>
+        </div>
+
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS34pAB6ZmuEXmBsJfXvFpybztoWG4XtpcvrpQdRyEj4zLXaEZAT9Q2_yPavI4sqlOJKys&usqp=CAU" />
+      </section>
+    </div>
   )
 }
