9dff228ca9056767bff2090e0c148cfe58488a00
Anish De <63192115+AnishDe12020@users.noreply.github.com>
2023-10-21 12:34:54 +0530

    fix: tier get function, maths bruh

--- lib/tiers.ts
+++ lib/tiers.ts
@@ -1,11 +1,11 @@
 export const getTier = (points: number) => {
-  if (points > 50) {
+  if (points >= 50) {
     return "DIAMOND"
-  } else if (points > 25) {
+  } else if (points >= 25) {
     return "PLATINUM"
-  } else if (points > 10) {
+  } else if (points >= 10) {
     return "GOLD"
-  } else if (points > 5) {
+  } else if (points >= 5) {
     return "SILVER"
   } else {
     return "BRONZE"
